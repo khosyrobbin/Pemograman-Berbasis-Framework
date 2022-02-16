696dfa8aacf7b4475885a16a6cdbe66a558ff2b3
khosyrobbin <khosybin@gmail.com>
2022-02-16 08:18:35 +0700

    5. arrow function 2

---
+++ pertemuan 1/Praktikum/arrowf.js
@@ -0,0 +1,10 @@
+// Tanpa arrow
+// const func = function (a, b) {
+//   return a + b;
+// };
+// alert(func(3, 3));
+// Menggunakan arrow
+const func = (a, b) => {
+    return a + b;
+  };
+  alert(func(5, 4));
