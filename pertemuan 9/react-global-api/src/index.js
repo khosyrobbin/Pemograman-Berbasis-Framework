5e4be5d847ecee9a6880541efcb6e3177c26d29e
khosyrobbin <khosybin@gmail.com>
2022-04-20 14:43:57 +0700

    Update Modul 9

--- pertemuan 9/react-global-api/src/index.js
+++ pertemuan 9/react-global-api/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import BlogPost from "./container/BlogPost/BlogPost";
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import BlogMahasiswa from "./container/BlogMahasiswa/BlogMahasiswa";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(<BlogPost/>, document.getElementById('content'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ pertemuan 9/react-global-api/src/services/index.js
@@ -0,0 +1,42 @@
+const domainPath = 'http://localhost:3001';
+const GetAPI = (path) => {
+    const promise = new Promise ((resolve, reject) => {
+        fetch(`${domainPath}/${path}`)
+            .then(response => response.json())
+            .then((result) => {
+                resolve(result);
+            }, (err) => {
+                reject(err);
+            })
+    })
+    return promise;
+}
+
+const PostAPI = (path, data) => {
+    const promise = new Promise ((resolve, reject) => {
+        fetch(`${domainPath}/${path}`, {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then((result)=> {
+                resolve(result);
+            }, (err)=> {
+                reject(err);
+            })
+    })
+    return promise;
+}
+
+const getNewsBlog = () => GetAPI('posts?_sort=id8_order=desc');
+const postNewsBlog = (dataYgDiKirim) => PostAPI('posts', dataYgDiKirim);
+
+const API = {
+    getNewsBlog,
+    postNewsBlog
+}
+
+export default API;
