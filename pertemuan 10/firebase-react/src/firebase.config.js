c01ed7680240427378b9992cf39078135fd6d569
khosyrobbin <khosybin@gmail.com>
2022-05-10 13:42:10 +0700

    update fix praktikum

---
+++ pertemuan 10/firebase-react/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAeM1yX_wienbhv6n7X0tmJ1_9RxfjYoDQ",
+    authDomain: "fir-react-e2891.firebaseapp.com",
+    projectId: "fir-react-e2891",
+    storageBucket: "fir-react-e2891.appspot.com",
+    messagingSenderId: "1015994771646",
+    appId: "1:1015994771646:web:1956fe332cf2384df2d6ef",
+    measurementId: "G-6XWVH67BX8"
+  };
