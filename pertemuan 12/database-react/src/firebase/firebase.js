12b7cf2e2b152590c4bb1c4ec1b29ff749b5117b
khosyrobbin <khosybin@gmail.com>
2022-05-25 12:57:52 +0700

    pertemuan 12

---
+++ pertemuan 12/database-react/src/firebase/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBGLj-guLdG65WqQIY_jxm_QwS2vxHw_iU",
+    authDomain: "pertemuan12-20c0f.firebaseapp.com",
+    databaseURL: "https://pertemuan12-20c0f-default-rtdb.firebaseio.com",
+    projectId: "pertemuan12-20c0f",
+    storageBucket: "pertemuan12-20c0f.appspot.com",
+    messagingSenderId: "443071076149",
+    appId: "1:443071076149:web:329b807df2ad70365d5e7c",
+    measurementId: "G-BQ0DS633BN"
+};
+
+export default firebaseConfig;
