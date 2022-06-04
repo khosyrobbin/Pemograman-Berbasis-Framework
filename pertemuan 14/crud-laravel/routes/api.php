dbd8dccd998e978bc892b297ada55f823c9b90f6
khosyrobbin <khosybin@gmail.com>
2022-06-04 15:50:19 +0700

    laravel crud

--- pertemuan 14/crud-laravel/routes/api.php
+++ pertemuan 14/crud-laravel/routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('create-student', 'App\Http\Controllers\StudentController@createStudent');
+Route::get('students', 'App\Http\Controllers\StudentController@studentsListing');
+Route::get('student/{id}', 'App\Http\Controllers\StudentController@studentDetail');
+Route::delete('student/{id}', 'App\Http\Controllers\StudentController@studentDelete');
