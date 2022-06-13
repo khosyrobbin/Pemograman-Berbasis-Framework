829d7f580661336ad4f30bb2245d3a3d24132743
khosyrobbin <khosybin@gmail.com>
2022-06-13 14:28:16 +0700

    edit home

--- UAS/shop-app/src/components/LogIn.js
+++ UAS/shop-app/src/components/LogIn.js
@@ -3,6 +3,11 @@ import { Redirect } from "react-router-dom";
 import { AuthContext } from "./Auth";
 import firebaseConfig from "../config.js";
 
+import { AwesomeButton } from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
+import Sepatu from "../img/shoes.png";
+
+
 const LogIn = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -21,8 +26,8 @@ const LogIn = () => {
         <>
             <h1>Log In</h1>
             <form onSubmit={handleSubmit}>
-                <label for="email">Email</label>
-                <input type="email" name="email" placeholder="Email" />
+                <label for="email"><AwesomeButton type="secondary">Email</AwesomeButton></label>
+                <input type="email" name="email" placeholder="Email"/>
                 <label for="password">Password</label>
                 <input type="password" name="password" placeholder="Password" />
                 <button type="submit">Submit</button>
