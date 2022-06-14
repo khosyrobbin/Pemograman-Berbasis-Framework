8d4b2e2353f91d67ab23381c19433e0c05543aba
Zwarzen <akurisang12@gmail.com>
2022-06-14 16:57:19 +0700

    desain Home dan Log in

--- UAS/shop-app/src/components/Home.js
+++ UAS/shop-app/src/components/Home.js
@@ -9,38 +9,10 @@ const Home = () => {
     const { currentUser } = useContext(AuthContext);
     return (
         <>
-            {/* <div className="container">
-                <div className="row align-items-right">
-                    <div className="col-auto pr-5" style={{ width: 530 }}>
-                        <h1 className="line-height-1 mt-5 mb-3">
-                            Selamat Datang di <br />
-                            Toko Dewa Kipas⚡️<br />
-                        </h1>
-                        <p
-                            className="mb-4 font-weight-light w-75"
-                            style={{ lineHeight: "170%" }}
-                        >
-                            Kipas kita <br /> Produk para Dewa
-                        </p>
-                        <Link to="/produk">
-                            <AwesomeButton type="primary">Cek Produk</AwesomeButton>
-                        </Link>
-                    </div>
-                    <div className="col-6 pl-5">
-                        <div style={{ width: 520, height: 250 }}>
-                            <img
-                                src={Sepatu}
-                                alt="Sepatu"
-                                className="img-fluid position-absolute"
-                                align="right"
-                                style={{ margin: "-30px 0 0 -30px" }}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div> */}
-
-            <h1>Home</h1>
+        <div className="home-page">
+            <center>
+            <h1>WELCOME!</h1>
+            <p>Do you want to Log In or Sign Up?</p>
             {currentUser ? (
                 <p>
                     You are logged - <Link to="/dashboard">View Dashboard</Link>
@@ -50,6 +22,8 @@ const Home = () => {
                     <Link to="/login">Log In</Link> or <Link to="/signup">Sign Up</Link>
                 </p>
             )}
+            </center>
+        </div>
         </>
     );
 };
