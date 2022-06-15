97ff231f74b67d5b84237b936bccccb8163a1254
Zwarzen <akurisang12@gmail.com>
2022-06-15 08:22:55 +0700

    desain navigation

--- UAS/shop-app/src/components/hero-banner/Hero-banner.js
+++ UAS/shop-app/src/components/hero-banner/Hero-banner.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Hero-banner.css';
 import firebaseConfig from '../../config';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
+import CartWindow from '../cart-window/Cart-window';
+import Navigation from '../navigation/Navigation';
 
 const HeroBanner = () => {
-
     return (
         <div>
-            <div className="hero-banner row">
-                <div className="col-3 left-banner"></div>
+            <div className="navigation row">
+                <div className="col-8 items">
+                    <ul>
+                        <li><Link to={`/`}>HOME</Link></li>
+                        <li><Link to={`/ShopList`}>SHOP</Link></li>
+                        <li><Link to={`/Profiles`}>PROFILE</Link></li>
+                        <li><Link onClick={() => firebaseConfig.auth().signOut()}>SIGN OUT</Link></li>
+                    </ul>
+                </div>
                 <div className="text col-6 ">
                     <h1>Neo OL Shop</h1>
                     <p>Find what you need easely in our shop!</p>
-                    <button onClick={() => firebaseConfig.auth().signOut()}>Sign out</button>
+                    {/* <button onClick={() => firebaseConfig.auth().signOut()}>Sign out</button> */}
                 </div>
-                <div className="col-3 right-banner"></div>
             </div>
-
-            
         </div>
 
     );
--- UAS/shop-app/src/components/home-container/Home-container.js
+++ UAS/shop-app/src/components/home-container/Home-container.js
@@ -4,10 +4,6 @@ import HeroBanner from "../hero-banner/Hero-banner";
 import ItemList from "../item-list/ItemList";
 import './Home-container.css';
 import Bootstrap from "bootstrap/dist/css/bootstrap.css";
-//import './App.css';
-// import Category from "../category/Category"
-// import Cart from "../cart/Cart";
-// import Product from "../product/Product";
 
 
 class HomeContainer extends Component {
